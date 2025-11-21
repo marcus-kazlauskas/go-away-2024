96bd3eb8ba5c6e5d93367c57150e9f510165957c
KozlovMV <kozlov.mihael@yandex.ru>
2025-11-21 18:37:01 +0300

    Update local-env

--- local-env/postgres/init.sql
+++ local-env/postgres/init.sql
@@ -2,10 +2,10 @@
 
 CREATE USER "aoc-2024" WITH PASSWORD 'aoc-2024';
 
-CREATE DATABASE "aoc-2024";
+CREATE DATABASE "advent-of-code";
 
-GRANT ALL PRIVILEGES ON DATABASE "aoc-2024" TO "aoc-2024";
+GRANT ALL PRIVILEGES ON DATABASE "advent-of-code" TO "aoc-2024";
 
-\c "aoc-2024"
+\c "advent-of-code"
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp"
